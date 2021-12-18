8684c4e8006fe5043d534bf1968d3224dd5c8455
nonpawitJT <ni.nonpawit_st@tni.ac.th>
2021-12-18 09:39:15 +0700

    ReactRouter

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import React from "react";
 import Footer from "./components/Footer";
 import Homepage from "./pages/Homepage";
 import NavBar from "./components/NavBar";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Aboutpage from "./pages/Aboutpage";
 
-function App() {
+function App() {  
   return (
-    <>
-    <NavBar/>
-    <Homepage/>
-    <Footer/>
+    <Router>
+      <NavBar/>
+      <Switch>
+        <Route exact path='/'><Homepage/></Route>
+        <Route path='/about'><Aboutpage/></Route>
+      </Switch>
+      <Footer/>
  
-    </>
+    </Router>
   );
 }
 
---
+++ src/pages/Aboutpage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Aboutpage = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <h2>About</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Aboutpage
